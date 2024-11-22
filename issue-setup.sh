ef71017c15191f81bf4af1dcfa2a0872630454b6
Fritz Lehnert <13189449+Neudrino@users.noreply.github.com>
2024-11-22 18:35:32 +0100

    change title based on CI

--- issue-setup.sh
+++ issue-setup.sh
@@ -2,11 +2,17 @@ set -x
 
 date=$(date -I)
 
+if ${GITHUB_ACTIONS}; then
+    title_suffix="on ${date}"
+else
+    title_suffix=""
+fi
+
 # Create new issue on project with initial instructions
 if gh issue view 1; then
     gh issue edit 1 \
         --body-file "issue-01-body.md" \
-        --title "Welcome to the Github Projects demo presentation on ${date}"
+        --title "Welcome to the Github Projects demo presentation ${title_suffix}"
 else
     gh issue create \
         --assignee "@me" \
