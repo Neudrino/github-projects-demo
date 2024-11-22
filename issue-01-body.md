0379b6d0b5451654f798c164c4e16a0393b33507
Fritz Lehnert <13189449+Neudrino@users.noreply.github.com>
2024-11-22 17:27:41 +0100

    add script and run it

---
+++ issue-01-body.md
@@ -0,0 +1,16 @@
+# by @Neudrino
+
+## Agenda
+
+1. Create a Github Project.
+   - Link the project with the current repository.
+2. Create some Github Issues.
+   - Explore (selected) Github Issue features.
+
+## What now?
+
+**Push a commit to the repository!**
+
+```bash
+git commit --allow-empty -n -m "Start the Github demonstration!"
+```
---
+++ project-setup.sh
@@ -0,0 +1,40 @@
+set -x
+
+# Owner default is "@me" and does not usually need to be specified
+owner=${GITHUB_USER:-"Neudrino"}
+repo="github-projects-demo"
+my_title="Github Projects and Issues Demo"
+date=$(date -I)
+
+# Create new issue on project with initial instructions
+if gh issue view 1; then
+    gh issue edit 1 \
+        --body-file "issue-01-body.md" \
+        --title "Welcome to the Github Projects demo presentation on ${date}"
+else
+    gh issue create \
+        --assignee "@me" \
+        --body-file "issue-01-body.md" \
+        --title "Welcome to the Github Projects demo presentation"
+fi
+
+# Create new project
+jq_expression=".projects[] | select(.title == \"${my_title}\") | .number"
+existing_project=$(gh project list --owner ${owner} --format json --jq "${jq_expression}")
+echo "${existing_project}"
+
+if [ -n "${existing_project}" ]; then
+    my_project_number=${existing_project}
+    echo "Project with >${my_title}< exists as number ${my_project_number}."
+else
+    echo "Creating new project with ${my_title}."
+    gh project create --owner ${owner} --title "${my_title}"
+    my_project_number=$(gh project list --owner ${owner} --format json --jq "${jq_expression}")
+fi
+
+# Link project to repository
+gh project link ${my_project_number} --owner ${owner} --repo ${repo}
+
+
+# gh project delete --owner ${owner} 4
+ 
